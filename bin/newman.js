1c482c34c5dc1dcabfc76b4ea6de9770b1142a14
Narayan Pai <narayanspai2000@gmail.com>
2020-06-29 11:58:31 +0530

    Add api-key-alias option, environment variable
    The option 'api-key-alias' and the environment variable 'NEWMAN_API_KEY_ALIAS' will help in specifying which alias
    of the API Key to use to fetch remote resources from Postman API

--- bin/newman.js
+++ bin/newman.js
@@ -37,6 +37,7 @@ program
     .option('--export-globals <path>', 'Specify an output file to dump Globals before exiting.')
     .option('--export-collection <path>', 'Specify an output file to save the executed collection')
     .option('--postman-api-key <apiKey>', 'API Key used to load the resources from the Postman API.')
+    .option('--api-key-alias <alias>', 'Specify the alias of the API Key to fetch remote resources using it.')
     .option('--delay-request [n]', 'Specify the extent of delay between requests (milliseconds)', util.cast.integer, 0)
     .option('--bail [modifiers]',
         'Specify whether or not to gracefully stop a collection run on encountering an error' +
